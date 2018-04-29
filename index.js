1be1320cbf4d6933b12b7889d900dbabf344e455
mouataz2003 <mouataz_bouazaoui@outlook.com>
2018-04-29 14:00:38 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const Discord = require =('discord.js')
+var prefix = '^'
+const ytdl = require('ytdl-core')
+const client = new Discord.Client();
+client.on('message', message => {
+if(!message.channel.guild) return;
+if (message.content.startsWith('$!play')) {
+message.delete(4000)
+const voiceChannel = message.member.voiceChannel;
+if (!voiceChannel) return message.reply(**You must be join to channel first**).then(msg => msg.delete(3000))
+voiceChannel.join()
+.then(connnection => {
+const stream = ytdl("الرابط من فضلك", { filter: 'audioonly' });
+const dispatcher = connnection.playStream(stream);
+dispatcher.on('end', () => voiceChannel.leave());
+return message.reply(":heavy_check_mark: انا اشغل الان").then(msg => msg.delete(3000))
+});
+}
